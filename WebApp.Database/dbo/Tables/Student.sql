e564c22934057a129fa4a335ef562ecaa6156729
Abdul Jabbar <abj@ciklum.com>
2020-04-22 00:36:23 +0500

    Initial Commit

---
+++ WebApp.Database/dbo/Tables/Student.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Student] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [Name]       NVARCHAR (MAX) NULL,
+    [CourseName] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Student] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ WebAppWithDatabase/Data/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppWithDatabase.Models;
+
+namespace WebAppWithDatabase.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext (DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<WebAppWithDatabase.Models.Student> Student { get; set; }
+    }
+}
